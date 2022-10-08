1cbfeec7b3420cbc19b8f0aa6b52e55548259b99
leo <leo091945@gmail.com>
2022-10-08 04:28:23 -0300

    some adds

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,36 +1,66 @@
 package engineTester;
 
 import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
 
+import entities.Camera;
+import entities.Entity;
+import entities.Light;
+import models.RawModel;
+import models.TextureModel;
 import renderingEngine.DisplayMannager;
 import renderingEngine.Loader;
-import renderingEngine.RawModel;
-import renderingEngine.Renderer;
+import renderingEngine.MasterRenderer;
+import renderingEngine.ObjLoader;
+import renderingEngine.EntityRenderer;
+import shaders.StaticShaders;
+import terrain.Terrain;
+import textures.ModelTexture;
 
 public class MainGameLoop {
 
 	public static void main(String[] args) {
 
+		
 		DisplayMannager.createDisplay();
 		Loader loader = new Loader();
-		Renderer renderer = new Renderer();
-		float[] vertices = { 
-				-0.5f, 0.5f,0,
-				-0.5f,-0.5f,0,
-				0.5f,-0.5f,0,
-				0.5f,0.5f,0
-		};
+		RawModel model  = ObjLoader.loadObject("stall", loader);
+		RawModel rawFern = ObjLoader.loadObject("fern", loader);
 		
-		int[] indices = {0,1,3,3,1,2};
-		RawModel model  = loader.loadtoVAO(vertices,indices);
-
+		TextureModel staticModel = new TextureModel(model, new ModelTexture(loader.loadTexture("stallTexture")));
+		TextureModel staticFern = new TextureModel(rawFern, new ModelTexture(loader.loadTexture("fern")));
+		
+		ModelTexture texture = staticModel.getTexture();
+		texture.setReflectivity(1);
+		texture.setShineDamper(10);
+		
+		ModelTexture fernTexture = staticFern.getTexture();
+		fernTexture.setReflectivity(0);
+		fernTexture.setShineDamper(100);
+		
+		Terrain terrain = new Terrain(0,-1,loader,new ModelTexture(loader.loadTexture("grass")));
+		Terrain terrain2 = new Terrain(1,-1,loader,new ModelTexture(loader.loadTexture("grass")));
+		
+		Entity entity = new Entity(staticModel, new Vector3f(0,0,-35),0,0,0,1);
+		Entity entity2 = new Entity(staticFern, new Vector3f(0,0,-5),0,0,0,1);
+		fernTexture.setHasTransparency(true);
+		fernTexture.setUseFakeLighting(true);
+		Light light = new Light(new Vector3f(5,-12,-33), new Vector3f(1,1,1));
+		Camera camera = new Camera();
+		
+		MasterRenderer mr = new MasterRenderer();
+		
 		while (!Display.isCloseRequested()) {
-			renderer.prepare();
-
+			camera.move();	
 			// game loop
-			renderer.render(model);
+			mr.processTerrain(terrain2);
+			mr.processTerrain(terrain);
+			mr.processEntity(entity);
+			mr.processEntity(entity2);
+			mr.render(light, camera);
 			DisplayMannager.updateDisplay();
 		}
+		mr.cleanUp();
 		loader.cleanUp();
 		DisplayMannager.closeDisplay();
 
--- src/renderingEngine/Loader.java
+++ src/renderingEngine/Loader.java
@@ -1,5 +1,8 @@
 package renderingEngine;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 import java.util.ArrayList;
@@ -10,25 +13,52 @@ import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL15;
 import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+
+import models.RawModel;
 
 public class Loader {
 	
 	private List<Integer> vaos = new ArrayList<Integer>();
 	private List<Integer> vbos = new ArrayList<Integer>();
+	private List<Integer> textures = new ArrayList<Integer>();
 	
-	public RawModel loadtoVAO(float[] positions,int[] indices) {
+	public RawModel loadtoVAO(float[] positions,float[] textureCoord,float[] normals, int[] indices) {
 		int vaoID = createVAO();
 		bindIndicesBuffer(indices);
-		storeDataInAttributeList(0,positions);
+		storeDataInAttributeList(0,3,positions);
+		storeDataInAttributeList(1,2,textureCoord);
+		storeDataInAttributeList(2,3,normals);
 		unbindVAO();
 		return new RawModel(vaoID, indices.length);
 	}
+	
+	public int loadTexture(String fileName) {
+		Texture texture = null;
+		
+		try {
+			texture = TextureLoader.getTexture("PNG", new FileInputStream("res\\" + fileName + ".png"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		int textureId = texture.getTextureID();
+		textures.add(textureId);
+		return textureId;
+	}
+	
 	public void cleanUp() {
 		for(int vao : vaos) {
 			GL30.glDeleteVertexArrays(vao);
 		}
 		for(int vbo : vbos) {
 			GL15.glDeleteBuffers(vbo);
+		}
+		for(int text : textures) {
+			GL11.glDeleteTextures(text);
 		}
 	}
 	
@@ -39,18 +69,21 @@ public class Loader {
 		return vaoID;
 		
 	}
-	private void storeDataInAttributeList(int attributeNumber, float[] data) {
+	
+	private void storeDataInAttributeList(int attributeNumber,int coordinateSize, float[] data) {
 		int vboID = GL15.glGenBuffers();
 		vbos.add(vboID);
 		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboID);
 		FloatBuffer buffer = storeDataInFloatBuffer(data);
 		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW);
-		GL20.glVertexAttribPointer(attributeNumber,3,GL11.GL_FLOAT,false,0,0);
+		GL20.glVertexAttribPointer(attributeNumber,coordinateSize,GL11.GL_FLOAT,false,0,0);
 		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
 	}
+	
 	private void unbindVAO() {
 		GL30.glBindVertexArray(0);
 	}
+	
 	private void bindIndicesBuffer(int[] indices) {
 		int vboID = GL15.glGenBuffers();
 		vbos.add(vboID);
@@ -58,16 +91,19 @@ public class Loader {
 		IntBuffer buffer = storeDataInIntBuffer(indices);
 		GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW);
 	}
+	
 	private IntBuffer storeDataInIntBuffer(int[] data) {
 		IntBuffer buffer = BufferUtils.createIntBuffer(data.length);
 		buffer.put(data);
 		buffer.flip();
 		return buffer;
 	}
+	
 	private FloatBuffer storeDataInFloatBuffer(float[] data) {
 		FloatBuffer buffer = BufferUtils.createFloatBuffer(data.length);
 		buffer.put(data);
 		buffer.flip();
 		return buffer;
 	}
+	
 }
